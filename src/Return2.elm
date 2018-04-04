e567d58b2094ec9a8ce5f3d7fd66ee88066a575c
Chadtech <chadstearns1@gmail.com>
2018-04-05 01:45:10 +0200

    Added documentation

--- src/Return2.elm
+++ src/Return2.elm
@@ -3,7 +3,6 @@ module Return2
         ( addCmd
         , addCmds
         , cmd
-        , incorp
         , mapCmd
         , mapModel
         , model
@@ -13,57 +12,143 @@ module Return2
         , withNoCmd
         )
 
+{-| This package makes it easier to build `(module, Cmd msg)`, the typical result of an update function
 
+
+# With
+
+@docs withCmd, withCmds, withNoCmd, withModel
+
+
+# Add
+
+@docs addCmd, addCmds
+
+
+# Map
+
+@docs mapModel, mapCmd
+
+
+# Get
+
+@docs model, cmd
+
+-}
+
+
+{-| Pack a cmd with your model
+
+    model
+        |> withCmd cmd
+
+-}
 withCmd : Cmd msg -> model -> ( model, Cmd msg )
 withCmd cmd model =
     ( model, cmd )
 
 
+{-| Pack multiple cmds with your model
+
+    model
+        |> withCmds [ cmd0, cmd1 ]
+
+-}
 withCmds : List (Cmd msg) -> model -> ( model, Cmd msg )
 withCmds cmds model =
     ( model, Cmd.batch cmds )
 
 
+{-| Pack a model with your cmd. This is useful if the business logic for your command is more complicated than the business logic for your model
+
+    Close ->
+        model.sessionId
+            |> Ports.Close
+            |> Ports.send
+            |> withModel model
+
+-}
 withModel : model -> Cmd msg -> ( model, Cmd msg )
 withModel =
     (,)
 
 
-addCmd : Cmd msg -> ( model, Cmd msg ) -> ( model, Cmd msg )
-addCmd newCmd ( model, cmd ) =
-    ( model, Cmd.batch [ newCmd, cmd ] )
+{-| Pack your model with no cmd
 
+    model
+        |> withNoCmd
 
-addCmds : List (Cmd msg) -> ( model, Cmd msg ) -> ( model, Cmd msg )
-addCmds newCmds ( model, cmd ) =
-    ( model, Cmd.batch [ Cmd.batch newCmds, cmd ] )
-
-
+-}
 withNoCmd : model -> ( model, Cmd msg )
 withNoCmd model =
     ( model, Cmd.none )
 
 
+{-| Sometimes you need to add a cmd to an already packaged model and cmd.
+
+    (model, cmd0)
+        |> addCmd cmd1
+
+        -- == (model, Cmd.batch [ cmd1, cmd0 ])
+
+-}
+addCmd : Cmd msg -> ( model, Cmd msg ) -> ( model, Cmd msg )
+addCmd newCmd ( model, cmd ) =
+    ( model, Cmd.batch [ newCmd, cmd ] )
+
+
+{-| Add many cmds to an already packaged model and cmd.
+
+    (model, cmd0)
+        |> addCmds [ cmd1, cmd2 ]
+
+        -- == (model, Cmd.batch [ cmd0, cmd1, cmd2 ])
+
+-}
+addCmds : List (Cmd msg) -> ( model, Cmd msg ) -> ( model, Cmd msg )
+addCmds newCmds ( model, cmd ) =
+    ( model, Cmd.batch [ Cmd.batch newCmds, cmd ] )
+
+
+{-| Ideally you wouldnt have to deconstruct a tupled model and cmd, but if you need to, this function does it.
+
+    Return2.model (model, cmd) == model
+
+-}
 model : ( model, Cmd msg )
 model =
     Tuple.first
 
 
+{-| Get the cmd of an already tupled model and cmd.
+
+    Return2.cmd (model, cmd) == cmd
+
+-}
 cmd : ( model, Cmd msg )
 cmd =
     Tuple.second
 
 
+{-| If you need to transform just the cmd in a tuple, such as if you need to wrap a sub-modules msg type
+
+    loginModel
+        |> Login.update subMsg
+        |> mapCmd LoginMsg
+
+-}
 mapCmd : (a -> b) -> ( model, Cmd a ) -> ( model, Cmd b )
 mapCmd f =
     Tuple.mapSecond (Cmd.map f)
 
 
+{-| If you need to transform just the model in a tuple, such as if you need to pack a submodel into the main model
+
+    loginModel
+        |> Login.update subMsg
+        |> mapModel (setPage model Page.Login)
+
+-}
 mapModel : (a -> b) -> ( a, Cmd msg ) -> ( b, Cmd msg )
 mapModel =
     Tuple.mapFirst
-
-
-incorp : model -> (subModel -> model -> model) -> ( subModel, Cmd msg ) -> ( model, Cmd msg )
-incorp model f ( subModel, cmd ) =
-    ( f subModel model, cmd )
--- src/Return3.elm
+++ src/Return3.elm
@@ -16,73 +16,207 @@ module Return3
         , withTuple
         )
 
+{-| This package makes it easier to build `(module, Cmd msg, reply)` a common return value for big sub-modules. See the readme for an explanation of what `Return3` is all about.
+
+
+# Return
+
+@docs Return
+
+
+# With
+
+@docs withReply, withNoReply, withTuple, withNothing
+
+
+# Add
+
+@docs addCmd, addCmds
+
+
+# Map
+
+@docs mapModel, mapCmd, mapReply
+
+
+# Get
+
+@docs model, cmd, reply
+
+
+# Incorporate
+
+@docs incorp
+
+-}
+
 import Return2 as R2
 
 
+{-| This alias formalizes that this particular triple is our return value.
+-}
 type alias Return model msg reply =
     ( model, Cmd msg, Maybe reply )
 
 
+{-| Add a reply to your tuple.
+
+    model
+        |> R2.withNoCmd
+        |> R3.withReply UserNameChanged
+
+-}
 withReply : reply -> ( model, Cmd msg ) -> Return model msg reply
 withReply reply ( model, cmd ) =
     ( model, cmd, Just reply )
 
 
+{-| Dont add a reply to your tuple.
+
+    FetchingData
+        |> R2.withCmd getLocalStorage
+        |> R3.withNoReply
+
+-}
 withNoReply : ( model, Cmd msg ) -> Return model msg reply
 withNoReply ( model, cmd ) =
     ( model, cmd, Nothing )
 
 
+{-| If building your reply takes a lot of work, use this function.
+
+    calculate width height x y
+        |> center model.size
+        |> UpdateSpace
+        |> R3.withTuple
+            (model |> R2.withNoCmd)
+
+-}
 withTuple : ( model, Cmd msg ) -> reply -> Return model msg reply
 withTuple ( model, cmd ) reply =
     ( model, cmd, Just reply )
 
 
+{-| Return the model with no cmd and no reply
+
+    model
+        |> withNothing
+
+-}
 withNothing : model -> Return model msg reply
 withNothing model =
     ( model, Cmd.none, Nothing )
 
 
+{-| Ideally you wouldnt have to deconstruct a `Return`, but if you need to, this function does it.
+
+    Return3.model (model, cmd, Nothing) == model
+
+-}
 model : Return model msg reply -> model
 model ( model, _, _ ) =
     model
 
 
+{-| Get the cmd of an already packed `Return`.
+
+    Return3.cmd (model, cmd, reply) == cmd
+
+-}
 cmd : Return model msg reply -> Cmd msg
 cmd ( _, cmd, _ ) =
     cmd
 
 
-reply : Return model msg reply -> reply
+{-| Get the reply of an already packed `Return`, if it exists
+
+    Return3.reply (model, cmd, maybeReply) == maybeReply
+
+-}
+reply : Return model msg reply -> Maybe reply
 reply ( _, _, reply ) =
     reply
 
 
+{-| Sometimes you need to add a cmd to an already packaged `Return`
+
+    (model, cmd0, reply)
+        |> addCmd cmd1
+
+        -- == (model, Cmd.batch [ cmd1, cmd0 ], reply)
+
+-}
 addCmd : Cmd msg -> Return model msg reply -> Return model msg reply
 addCmd newCmd ( model, cmd, reply ) =
     ( model, Cmd.batch [ newCmd, cmd ], reply )
 
 
+{-| Add many cmds to an already packaged `Return`
+
+    (model, cmd0, reply)
+        |> addCmds [ cmd1, cmd2 ]
+
+        -- == (model, Cmd.batch [ cmd0, cmd1, cmd2 ], reply)
+
+-}
 addCmds : List (Cmd msg) -> Return model msg reply -> Return model msg reply
 addCmds newCmds ( model, cmd, reply ) =
     ( model, Cmd.batch [ Cmd.batch newCmds, cmd ], reply )
 
 
+{-| If you need to transform just the model in a `Return`, such as if you need to pack a submodel into the main model
+
+    loginModel
+        |> Login.update subMsg
+        |> mapModel (setPage model Page.Login)
+
+-}
 mapModel : (a -> b) -> Return a msg reply -> Return b msg reply
 mapModel f ( model, cmd, reply ) =
     ( f model, cmd, reply )
 
 
+{-| If you need to transform just the cmd in a `Return`, such as if you need to wrap a sub-modules msg type
+
+    loginModel
+        |> Login.update subMsg
+        |> mapCmd LoginMsg
+
+-}
 mapCmd : (a -> b) -> Return model a reply -> Return model b reply
 mapCmd f ( model, cmd, reply ) =
     ( model, Cmd.map f cmd, reply )
 
 
-mapReply : (Maybe reply -> Maybe reply) -> Return model msg reply -> Return model msg reply
+{-| -}
+mapReply : (Maybe a -> Maybe b) -> Return model msg a -> Return model msg b
 mapReply f ( model, cmd, reply ) =
     ( model, cmd, f reply )
 
 
+{-| `Return`s contain a reply, and that reply needs to be handled much like a `msg` does in an update function.
+
+    loginModel
+        |> Login.update loginMsg
+        |> R3.mapCmd LoginMsg
+        |> incorp handleLoginReply model
+
+
+    handleLoginReply : Login.Model -> Maybe Reply -> Model -> (Model, Cmd Msg)
+    handleLoginreply loginModel maybeReply model =
+        case maybeReply of
+            Nothing ->
+                { model | page = Login loginModel }
+                    |> R2.withNoCmd
+
+            Just (LoggedIn user) ->
+                { model
+                    | page = Home Home.init
+                    , user = Just user
+                }
+                    |> R2.withNoCmd
+
+-}
 incorp : (subModel -> Maybe reply -> model -> ( model, Cmd msg )) -> model -> Return subModel msg reply -> ( model, Cmd msg )
 incorp f model ( subModel, cmd, reply ) =
     f subModel reply model
